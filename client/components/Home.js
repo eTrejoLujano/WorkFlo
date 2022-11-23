91c3c67a23644d8e4a230640a7359ea0bf15a4b4
Matt Bruer <mbruer@gmail.com>
2022-11-23 10:11:05 -0600

    set up socket

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,17 +1,35 @@
 import React from "react";
 import { connect } from "react-redux";
 import VideoChat from "./VideoChat";
+import styled from "styled-components";
+import socket from "../socket";
+import Classroom from "./Classroom";
+import { Link } from "react-router-dom";
 
 /**
  * COMPONENT
  */
+
+const dummyClasses = ["Fullstack Academy", "History", "Math"];
+
 export const Home = (props) => {
   const { username } = props;
+  console.log("socket", socket);
+  socket.emit("new-message", `${username} is connected to the socket`);
 
   return (
     <div>
       <h3>Welcome, {username}</h3>
-      <VideoChat />
+      {/* <VideoChat /> */}
+
+      <h2>Your classes</h2>
+      <ClassesContainer>
+        {dummyClasses.map((c, i) => (
+          <Link key={i} to="/classroom">
+            <p>{c}</p>
+          </Link>
+        ))}
+      </ClassesContainer>
     </div>
   );
 };
@@ -26,3 +44,7 @@ const mapState = (state) => {
 };
 
 export default connect(mapState)(Home);
+
+const ClassesContainer = styled.div`
+  border: 1px solid black;
+`;
