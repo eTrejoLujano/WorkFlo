3669ccb84b5e4ffeea833638ac6031f66e4de113
Matt Bruer <mbruer@gmail.com>
2022-11-23 10:13:56 -0600

    changed back

--- server/api/index.js
+++ server/api/index.js
@@ -4,7 +4,7 @@ module.exports = router;
 router.use("/users", require("./users"));
 
 router.use((req, res, next) => {
-  const err = new Error("Not Found");
-  err.status = 404;
-  next(err);
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
 });
