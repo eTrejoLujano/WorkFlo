40b4cd6e9998716b06e35776beab8e31c19e3b80
Matt Bruer <mbruer@gmail.com>
2022-11-23 10:12:51 -0600

    final sockets

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,10 @@
-const router = require('express').Router()
-module.exports = router
+const router = require("express").Router();
+module.exports = router;
 
-router.use('/users', require('./users'))
+router.use("/users", require("./users"));
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found')
-  error.status = 404
-  next(error)
-})
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
