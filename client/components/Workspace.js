55a1c3af8040ac96a3cd3bafb04199bd756e0990
Jerral Graham <jerralgraham@yahoo.com>
2022-12-01 05:45:16 -0500

    updated copy button

--- client/components/Workspace.js
+++ client/components/Workspace.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchLists } from "../store/listSlice";
 import AddAnotherButton from "./AddAnotherButton";
 import List from "./List";
 import AddList from "./AddList";
+import CopyLinkModal from './CopyLinkModal'
 
 function Workspace() {
   const dispatch = useDispatch();
@@ -13,9 +14,24 @@ function Workspace() {
   useEffect(() => {
     dispatch(fetchLists(params.projectId));
   }, []);
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const [value, setValue] = useState('')
+
+  const buttonClicked = ()=> {
+    setModalOpen(true)
+    setValue(window.location.href)
+  }
+
   return (
     <div>
+    <div className="workspace-heading">
       <h2>This Is The Workspace</h2>
+      <button className="inviteBtn" onClick={buttonClicked}>+ Invite</button>
+
+      {modalOpen && <CopyLinkModal setOpenModal={setModalOpen} value={value} />}
+    </div>
+
       <div style={styles.listsContainer}>
         {lists.length &&
           lists.map((list) => (
