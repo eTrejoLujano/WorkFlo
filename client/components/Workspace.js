b5294e05490bb5f75b5de3cbc9a33c80c6744f6a
Jerral Graham <jerralgraham@yahoo.com>
2022-12-01 11:38:53 -0500

    legal

--- client/components/Workspace.js
+++ client/components/Workspace.js
@@ -1,56 +1,11 @@
+import React from 'react'
 
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { fetchLists } from "../store/listSlice";
-import AddAnotherButton from "./AddAnotherButton";
-import List from "./List";
-import AddList from "./AddList";
-import CopyLinkModal from './CopyLinkModal'
-
-function Workspace() {
-  const dispatch = useDispatch();
-  const lists = useSelector((state) => state.lists);
-  const params = useParams();
-  useEffect(() => {
-    dispatch(fetchLists(params.projectId));
-  }, []);
-
-  const [modalOpen, setModalOpen] = useState(false);
-  const [value, setValue] = useState('')
-
-  const buttonClicked = ()=> {
-    setModalOpen(true)
-    setValue(window.location.href)
-  }
-
+function WorkSpace() {
   return (
     <div>
-    <div className="workspace-heading">
-      <h2>This Is The Workspace</h2>
-      <button className="inviteBtn" onClick={buttonClicked}>+ Invite</button>
 
-      {modalOpen && <CopyLinkModal setOpenModal={setModalOpen} value={value} />}
     </div>
-
-      <div style={styles.listsContainer}>
-        {lists.length &&
-          lists.map((list) => (
-            <List key={list.id} title={list.title} cards={list.cards} />
-          ))}
-        <AddList />
-        {/* <AddAnotherButton list /> */}
-      </div>
-    </div>
-  );
+  )
 }
 
-
-const styles = {
-  listsContainer: {
-    display: "flex",
-    flexDirection: "row",
-  },
-};
-
-export default Workspace;
+export default WorkSpace
