06803ed47d355f6cd830540ad95ea4510a3dd9ad
Erik Trejo Lujano <85711028+eTrejoLujano@users.noreply.github.com>
2022-12-02 07:35:01 -0800

    Update Workspace.js

--- client/components/Workspace.js
+++ client/components/Workspace.js
@@ -1,38 +1,11 @@
-// import React, { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { useParams } from "react-router-dom";
-// import { fetchLists } from "../store/listSlice";
-// import AddAnotherButton from "./AddAnotherButton";
-// import List from "./List";
-// import AddList from "./AddList";
+import React from 'react'
 
-// function Workspace() {
-//   const dispatch = useDispatch();
-//   const lists = useSelector((state) => state.lists);
-//   const params = useParams();
-//   useEffect(() => {
-//     dispatch(fetchLists(params.projectId));
-//   }, []);
-//   return (
-//     <div>
-//       <h2>This Is The Workspace</h2>
-//       <div style={styles.listsContainer}>
-//         {lists.length &&
-//           lists.map((list) => (
-//             <List key={list.id} title={list.title} cards={list.cards} />
-//           ))}
-//         <AddList />
-//         {/* <AddAnotherButton list /> */}
-//       </div>
-//     </div>
-//   );
-// }
+function WorkSpace() {
+  return (
+    <div>
 
-// const styles = {
-//   listsContainer: {
-//     display: "flex",
-//     flexDirection: "row",
-//   },
-// };
+    </div>
+  )
+}
 
-// export default Workspace;
+export default WorkSpace
