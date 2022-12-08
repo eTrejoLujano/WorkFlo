cf2423af61df5bc2fb58be05380b7b536db89e49
Matt Bruer <mbruer@gmail.com>
2022-12-08 17:40:17 -0600

    started chat

--- client/store/chatSlice.js
+++ client/store/chatSlice.js
@@ -3,19 +3,38 @@ import axios from "axios";
 import history from "../history";
 import { sendToken } from "./helperFunctions";
 
+export const sendMessage = createAsyncThunk(
+  "chat/sendMessage",
+  async (message, thunkAPI) => {
+    try {
+      const { data } = await axios.post("/api/chat", message, sendToken());
+      thunkAPI.dispatch(sentMessage(data));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
 const initialState = {
   online: [],
+  messageList: [],
 };
 
 const chatSlice = createSlice({
-  name: "ui",
+  name: "chat",
   initialState,
   reducers: {
     userJoined: (state, action) => {
       state.online.push(action.payload);
     },
+    sentMessage: (state, action) => {
+      state.messageList.push(action.payload);
+    },
+    receiveMessage: (state, action) => {
+      state.messageList.push(action.payload);
+    },
   },
 });
 
-export const { userJoined } = chatSlice.actions;
+export const { userJoined, sentMessage, receiveMessage } = chatSlice.actions;
 export default chatSlice.reducer;
--- server/db/models/Message.js
+++ server/db/models/Message.js
@@ -5,12 +5,18 @@ const User = require("../db");
 const Message = db.define(
   "message",
   {
+    author: {
+      type: Sequelize.STRING,
+    },
     message: {
       type: Sequelize.STRING,
       allowNull: false,
       validate: {
         notEmpty: true,
       },
+    },
+    time: {
+      type: Sequelize.STRING,
     },
   },
   {
