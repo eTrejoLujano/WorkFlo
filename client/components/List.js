c542302f534763dc031c703b3274855b83200a77
Erik Trejo Lujano <85711028+eTrejoLujano@users.noreply.github.com>
2022-12-11 22:50:03 -0800

    fixed issues for deployment

--- client/components/List.js
+++ client/components/List.js
@@ -3,7 +3,6 @@ import { useSelector } from "react-redux";
 import { Droppable, Draggable } from "react-beautiful-dnd";
 import { useParams } from "react-router-dom";
 
-import "../styles/List.css";
 import AddSingleCard from "./AddSingleCard";
 import SingleCard from "./SingleCard";
 
