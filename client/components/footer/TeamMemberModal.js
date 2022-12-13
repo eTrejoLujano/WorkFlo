6aa03f870f4101cce2b4521a3711fe86161a865b
Peter Rodocker <107810098+PeterRodocker@users.noreply.github.com>
2022-12-13 08:42:52 -0800

    fixed seed data

--- client/components/footer/TeamMemberModal.js
+++ client/components/footer/TeamMemberModal.js
@@ -5,22 +5,22 @@ import Modal from "../ReusableModal";
 
 const teamInfo = {
   matt: {
-    content: "Hi, I am Matt's content",
+    content: "Matt Bruer - Full Stack Developer",
     linkedIn: "https://www.linkedin.com/in/matt-bruer/",
     github: "https://github.com/mattbruer",
   },
   jerral: {
-    content: "Hi, I am Jerral's content",
+    content: "Jerral Graham - Full Stack Developer",
     linkedIn: "https://www.linkedin.com/in/jerral-graham/",
     github: "https://github.com/jerrol3000",
   },
   erik: {
-    content: "Hi, I am Erik's content",
+    content: "Erik Trejo - Full Stack Developer",
     linkedIn: "https://www.linkedin.com/in/erik-trejo-lujano/",
     github: "https://github.com/eTrejoLujano",
   },
   peter: {
-    content: "Hi, I am Peter's content",
+    content: "Peter Rodocker - Full Stack Developer",
     linkedIn: "https://www.linkedin.com/in/peterrodocker/",
     github: "https://github.com/PeterRodocker",
   },
